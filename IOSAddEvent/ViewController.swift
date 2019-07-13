2dd958d1cbe1fff25c58012f933f84a1aeeb690b
Eric PAJOT <contact@pajot.ch>
2019-07-13 21:55:48 +0200

    Initial Commit

---
+++ IOSAddEvent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSAddEvent
+//
+//  Created by Eric PAJOT on 13.07.19.
+//  Copyright © 2019 Eric PAJOT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
