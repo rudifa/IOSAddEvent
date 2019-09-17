ae0287706ad6cbc87bf7c07c88c2eb19f05bd038
rudifa <rudi.farkas@gmail.com>
2019-09-17 22:40:50 +0200

    CalEventManager: remove old style methods

--- IOSAddEvent/Controller/MainViewController.swift
+++ IOSAddEvent/Controller/MainViewController.swift
@@ -10,7 +10,7 @@ import EventKit
 import UIKit
 
 class MainViewController: UIViewController {
-    var userName = "rf" // EP'S
+    var userName = "rf add event"
 
     @IBOutlet var calendarSelector: UITextField!
 
@@ -20,7 +20,7 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        calEventManager.getAllCalendars { result in
+        calEventManager.getCalendars { result in
             switch result {
             case let .success(calendars):
                 DispatchQueue.main.async {
@@ -28,28 +28,16 @@ class MainViewController: UIViewController {
                     self.calendarSelector.loadDropdownData(data: calendarTitles, selectionHandler: self.onSelect)
                 }
             case let .failure(error):
-                self.printClassAndFunc(info: "getAllCalendars error: \(error)")
+                self.printClassAndFunc(info: "getCalendars error: \(error)")
             }
         }
-    }
-
-    override func shouldPerformSegue(withIdentifier identifier: String, sender _: Any?) -> Bool {
-        if identifier == "segueToCalEventsTableViewController" {
-            guard calEventManager.getCalendar(title: selectedCalendarTitle) != nil else {
-                printClassAndFunc(info: "calendar \(selectedCalendarTitle) not found")
-                return false
-            }
-            printClassAndFunc(info: "calendar \(selectedCalendarTitle) found")
-            return true
-        }
-        return false
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender _: Any?) {
         printClassAndFunc(info: "segue.identifier \(String(describing: segue.identifier))")
         if segue.identifier == "segueToCalEventsTableViewController" {
             let destination = segue.destination as! CalEventsTableViewController
-            calEventManager.getEventsFromCalendar(title: selectedCalendarTitle) { result in
+            calEventManager.getEventsFrom(calendar: selectedCalendarTitle) { result in
                 switch result {
                 case let .success(events):
                     self.printClassAndFunc(info: "events.count= \(events.count)")
@@ -76,105 +64,21 @@ class MainViewController: UIViewController {
 
     @IBAction func addEventBtnPressed(_: Any) {
         let calEventManager = CalEventManager.shared
-        calEventManager.insertCalEvent(userName: "rf+1", calendarTitle: "Code_Cal") { result in
+        calEventManager.addEvent(title: userName, into: "Code_Cal") { result in
             var message = ""
             switch result {
-            case let .success(event):
-                self.printClassAndFunc(info: "success: \(event.brief)")
-                message = "\(event.brief)"
-                break
+            case .success:
+                self.printClassAndFunc(info: "success)")
+                message = ""
             case let .failure(error):
                 self.printClassAndFunc(info: "error: \(error)")
                 message = "Error: \(error)"
-                break
             }
             DispatchQueue.main.async {
                 let alert = UIAlertController(title: "Add event", message: message, preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
                 self.present(alert, animated: true)
             }
-        }
-    }
-
-    @IBAction func addEventBtnPressed_0(_: Any) {
-        let eventStore = EKEventStore()
-        switch EKEventStore.authorizationStatus(for: .event) {
-        case .authorized:
-            insertEvent(store: eventStore)
-        case .denied:
-            printClassAndFunc(info: "Access denied")
-        case .notDetermined:
-            eventStore.requestAccess(to: .event, completion: { [weak self] (granted: Bool, _: Error?) -> Void in
-                if granted {
-                    self!.insertEvent(store: eventStore)
-                } else {
-                    self?.printClassAndFunc(info: "Access denied")
-                }
-            })
-        default:
-            printClassAndFunc(info: "Case default")
-        }
-    }
-
-    func insertEvent(store: EKEventStore) {
-        let calendars = store.calendars(for: .event)
-
-        for calendar in calendars {
-//            printClassAndFunc(info: "Calendar\(calendar.title)")
-            if calendar.title == "Code_Cal" {
-                let startDate = Date()
-                let endDate = startDate.addingTimeInterval(1 * 60 * 60)
-                let event = EKEvent(eventStore: store)
-                event.calendar = calendar
-
-                event.title = userName // EP
-                event.startDate = startDate
-                event.endDate = endDate
-                do {
-                    try store.save(event, span: .thisEvent)
-                } catch {
-                    printClassAndFunc(info: "Error saving event in calendar")
-                }
-            }
-        }
-    }
-
-    // EP's Try
-
-    @IBAction func addCalBtnPressed(_: Any) {
-        // Create an Event Store instance
-        let eventStore = EKEventStore()
-
-        // Use Event Store to create a new calendar instance
-        // Configure its title
-        let newCalendar = EKCalendar(for: .event, eventStore: eventStore)
-
-        // Probably want to prevent someone from saving a calendar
-        // if they don't type in a name...
-        newCalendar.title = "Code_Cal_01"
-
-        // Access list of available sources from the Event Store
-        let sourcesInEventStore = eventStore.sources
-
-        // Filter the available sources and select the "Local" source to assign to the new calendar's
-        // source property
-        newCalendar.source = sourcesInEventStore.filter {
-            (source: EKSource) -> Bool in
-            source.sourceType.rawValue == EKSourceType.local.rawValue
-        }.first!
-
-        // Save the calendar using the Event Store instance
-        do {
-            try eventStore.saveCalendar(newCalendar, commit: true)
-            UserDefaults.standard.set(newCalendar.calendarIdentifier, forKey: "EventTrackerPrimaryCalendar")
-            navigationController?.popViewController(animated: true)
-            // self.dismiss(animated: true, completion: nil)
-        } catch {
-            let alert = UIAlertController(title: "Calendar could not save", message: (error as NSError).localizedDescription, preferredStyle: .alert)
-            let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-            alert.addAction(OKAction)
-            navigationController?.popViewController(animated: true)
-            // self.present(alert, animated: true, completion: nil)
         }
     }
 }
--- IOSAddEvent/Manager/CalEventManager.swift
+++ IOSAddEvent/Manager/CalEventManager.swift
@@ -1,5 +1,5 @@
 //
-//  CalEventManager.swift v.0.2.4
+//  CalEventManager.swift v.0.3.0
 //  IOSAddEvent
 //
 //  Created by Rudolf Farkas on 05.09.19.
@@ -223,159 +223,7 @@ class CalEventManager: NSObject {
         }
     }
 
-    // MARK: - old style methods
-
-    private func updateAuthorization() {
-        switch EKEventStore.authorizationStatus(for: .event) {
-        case .authorized:
-            authorized = true
-            printClassAndFunc(info: "authorized")
-        case .denied:
-            authorized = false
-            printClassAndFunc(info: "authorization denied")
-        case .notDetermined:
-            eventStore.requestAccess(to: .event, completion: { (granted: Bool, _: Error?) -> Void in
-                if granted {
-                    self.authorized = true
-                    self.printClassAndFunc(info: "authorization granted")
-                } else {
-                    self.authorized = false
-                    self.printClassAndFunc(info: "authorization denied")
-                }
-            })
-        default:
-            authorized = false
-            printClassAndFunc(info: "authorization not obtained")
-        }
-    }
-
-    private func getEventsFromCalendarAuthorized(title: String, completion: ((Result<[EKEvent], CalEventError>) -> Void)) {
-        guard let calendar = getCalendar(title: title) else {
-//            printClassAndFunc(info: "*** calendar \(title) not found")
-            completion(.failure(.calendarNotFound))
-            return
-        }
-        printClassAndFunc(info: "calendar \(title) found")
-        let events = getEvents(calendar: calendar)
-        completion(.success(events))
-    }
-
-    /// Gets events from calendar
-    ///
-    /// - Parameter title: calendar title
-    func getEventsFromCalendar(title: String, completion: @escaping ((Result<[EKEvent], CalEventError>) -> Void)) {
-        getAllCalendars { result in
-            switch result {
-            case let .success(calendars):
-                self.printClassAndFunc(info: "calendars.count= \(calendars.count)")
-                let filtered = calendars.filter({ $0.title == title })
-                if filtered.count > 0 {
-                    let calendar = filtered[0]
-                    self.printClassAndFunc(info: "calendar \(calendar.title) found")
-                    let events = self.getEvents(calendar: calendar)
-                    self.printClassAndFunc(info: "events.count= \(events.count)")
-                    completion(.success(events))
-                } else {
-                    self.printClassAndFunc(info: "calendar \(title) not found")
-                    completion(.failure(.calendarNotFound))
-                }
-            case let .failure(error):
-                self.printClassAndFunc(info: "error= \(error)")
-                completion(.failure(error))
-            }
-        }
-    }
-
-    /// Check authorization for adding calendar events and insert event into calendar
-    ///
-    /// - Parameters:
-    ///   - userName: user name
-    ///   - calendarTitle: calendar title, e.g. "Code_Cal"
-    func insertCalEvent(userName: String, calendarTitle: String, completion: @escaping ((Result<EKEvent, CalEventError>) -> Void)) {
-        if authorized {
-            printClassAndFunc(info: "AUTHORIZED")
-            insertEvent(store: eventStore, userName: userName, calendarTitle: calendarTitle, completion: completion)
-        } else {
-            switch EKEventStore.authorizationStatus(for: .event) {
-            case .authorized:
-                authorized = true
-                insertEvent(store: eventStore, userName: userName, calendarTitle: calendarTitle, completion: completion)
-            case .denied:
-                //            printClassAndFunc(info: "Access denied")
-                completion(.failure(.accessDenied))
-            case .notDetermined:
-                eventStore.requestAccess(to: .event, completion: { (granted: Bool, _: Error?) -> Void in
-                    if granted {
-                        self.authorized = true
-                        self.insertEvent(store: self.eventStore, userName: userName, calendarTitle: calendarTitle, completion: completion)
-                    } else {
-                        //                    self?.printClassAndFunc(info: "Access denied")
-                        completion(.failure(.accessDenied))
-                    }
-                })
-            default:
-                //            printClassAndFunc(info: "Case default")
-                completion(.failure(.unknownError))
-            }
-        }
-    }
-
-    /// Insert event into calendar
-    ///
-    /// - Parameters:
-    ///   - userName: user name
-    ///   - calendarTitle: calendar title, e.g. "Code_Cal"
-    private func insertEvent(store: EKEventStore, userName: String, calendarTitle: String, completion: ((Result<EKEvent, CalEventError>) -> Void)) {
-        //        printClassAndFunc(info: "")
-
-        let calendars = store.calendars(for: .event)
-
-        for calendar in calendars {
-//            printClassAndFunc(info: "Calendar: \(calendar)")
-            if calendar.title == calendarTitle {
-                let startDate = Date()
-                let endDate = startDate.addingTimeInterval(2 * 60 * 60)
-
-                let event = EKEvent(eventStore: store)
-                event.calendar = calendar
-
-                event.title = userName
-                event.startDate = startDate
-                event.endDate = endDate
-
-                do {
-                    try store.save(event, span: .thisEvent)
-                    completion(.success(event))
-                    return
-                } catch {
-                    completion(.failure(.failedToSaveEventInCalendar))
-                    return
-                }
-            }
-        }
-        completion(.failure(.calendarNotFound))
-    }
-
-    /// Get calendars from the event store
-    ///
-    /// - Returns: calendars in the event store
-    func getCalendars() -> [EKCalendar] {
-        return eventStore.calendars(for: .event)
-    }
-
-    /// Find calendar by its title
-    ///
-    /// - Parameter title:
-    /// - Returns: calendar, or nil if not found
-    func getCalendar(title: String) -> EKCalendar? {
-        let calendars = getCalendars()
-        let calendars1 = calendars.filter({ $0.title == title })
-        if calendars1.count > 0 {
-            return calendars[0]
-        } else {
-            return nil
-        }
-    }
+    // MARK: - old style methods reused
 
     /// Load events from calendar
     ///
@@ -392,9 +240,7 @@ class CalEventManager: NSObject {
         let endDate = dateFormatter.date(from: "2019-12-31")
 
         if let startDate = startDate, let endDate = endDate {
-//            let eventStore = EKEventStore()
-
-            // Use an event store instance to create and properly configure an NSPredicate
+            // Use an event store instance to create a NSPredicate
             let eventsPredicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: [calendar])
 
             // Use the configured NSPredicate to find and return events in the store that match
@@ -402,40 +248,8 @@ class CalEventManager: NSObject {
                 (e1: EKEvent, e2: EKEvent) -> Bool in
                 return e1.startDate.compare(e2.startDate) == ComparisonResult.orderedAscending
             }
-
-//            printClassAndFunc(info: "events.count=\(events.count)")
-//            for event in events {
-//                printClassAndFunc(info: "events.count=\(event.brief)")
-//            }
             return events
         }
         return []
-    }
-
-    /// Check authorization for getting calendars and get all calendars
-    ///
-    func getAllCalendars(completion: @escaping ((Result<[EKCalendar], CalEventError>) -> Void)) {
-        switch EKEventStore.authorizationStatus(for: .event) {
-        case .authorized:
-            getAllCalendarsAuthorized(completion: completion)
-        case .denied:
-            completion(.failure(.accessDenied))
-        case .notDetermined:
-            eventStore.requestAccess(to: .event, completion: { (granted: Bool, _: Error?) -> Void in
-                if granted {
-                    self.getAllCalendarsAuthorized(completion: completion)
-                } else {
-                    completion(.failure(.accessDenied))
-                }
-            })
-        default:
-            completion(.failure(.unknownError))
-        }
-    }
-
-    private func getAllCalendarsAuthorized(completion: ((Result<[EKCalendar], CalEventError>) -> Void)) {
-        let calendars = eventStore.calendars(for: .event)
-        printClassAndFunc(info: "calendars.count=\(calendars.count)")
-        completion(.success(calendars))
     }
 }
